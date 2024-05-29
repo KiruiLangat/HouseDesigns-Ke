42425e41d2211ba68907693a2b5991e9e6806ddc
Kirui Langat <ted.kirui@students.ku.ac.ke>
2024-05-30 02:11:10 +0300

    updates

--- src/BrowseCarousel.js
+++ src/BrowseCarousel.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
+import Link from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
